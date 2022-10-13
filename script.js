8688a9b76a4c5c096f6f935365a6f51cbe19a0b5
guilherme3110 <guilherme311094@hotmail.com>
2022-10-13 20:13:44 -0300

    Exercicios de variaveis

---
+++ script.js
@@ -0,0 +1,84 @@
+<!-- questão 1 -->
+
+            let valorDaCompra = prompt("Quanto deseja abastecer");
+	        let valorLitro = parseFloat("6,80") ; 
+	
+	
+	        litros = (valorDaCompra / valorLitro);
+	
+	        alert(` O cliente abasteceu ${litros}L `)
+
+        <!-- questão 2 --> 
+
+            let celcius = prompt("Insira a temperatura em graus celcius");
+            let f = (9 * celcius + 160) / 5;
+
+            alert(` O valor ${celcius}c em Fahrenheit é ${f} `)
+
+        <!-- questão 3 --> 
+
+            let f = prompt("Insira a temperatura em fahrenheit");
+            let c = ((f - 32) * 5) / 9;
+
+            alert(` O valor ${f} em celcius é ${c}`)
+
+        <!-- questão 4 --> 
+
+            let altura = prompt("Digite a altura do retangulo");
+            let largura = prompt("Digite a largura do retangulo");
+            let comprimento = prompt("digite o comprimento do retangulo");
+
+            let volume = parseInt(comprimento * largura * altura);
+
+            alert(` O volume e de: ${volume} `) 
+
+        <!-- questão 5 --> 
+
+            let n1 = prompt("Insira um numero");
+            let n2 = prompt("insira outro numero");
+
+            quadrado = (n1 * n2);
+
+            alert(` o valor ${n1} ao quadrado é ${quadrado}`)
+
+
+        <!-- questão 6 --> 
+
+
+
+
+
+
+
+
+
+        <!-- questão 7 --> 
+
+            let n1 = prompt("digite um número inteiro");
+	        let n2 = prompt("digite um segundo número inteiro");
+	
+	        divisao = (n1 / n2 );
+	
+	        alert(` O resta da divisão de ${n1} por ${n2} é: ${divisao}`)
+
+        <!-- questão 8 --> 
+
+            let text = prompt("digite uma frase");
+            let length = text.length;
+
+            alert(` Este texto tem ${length} caracteres `)      
+
+        <!-- questão 9 --> 
+
+            
+
+
+
+
+
+
+
+        <!-- questão 10 --> 
+
+
+
